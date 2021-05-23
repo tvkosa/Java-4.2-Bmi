348d361da7a2a693e056c4a6f9f6c2094a362234
Tatiana Kosarevskaya <tv.kosarevskaya@gmail.com>
2021-05-23 17:50:48 +0300

    Main changing

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        BmiService calc = new BmiService();
+        BmiService service = new BmiService();
         double weight = 77.0; //вес в килограммах
         double height = 1.76; //рост в метрах
-        double index = calc.calculate(weight, height);
+        double index = service.calculate(weight, height);
         System.out.println("Индекс массы тела:" + index);
     }
 }
